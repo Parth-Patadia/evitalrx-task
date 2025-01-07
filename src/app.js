c579e7ad7784906235cd2c279f18657c450c66bc
Parth Patadia <parthpatadia78@gmail.com>
2025-01-08 00:44:46 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const profileRoutes = require('./routes/profile');
 const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
 const productRoutes = require('./routes/product');
+const queryRoutes = require('./routes/query');
 
 // Use routes
 app.use('/api', authRoutes);
@@ -21,8 +22,13 @@ app.use('/api', profileRoutes);
 app.use('/api', cartRoutes);
 app.use('/api', orderRoutes);
 app.use('/api', productRoutes);
+app.use('/api', queryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 }); 
+
+git add src/app.js
+git add src/controllers/queryController.js
+git add src/routes/query.js
